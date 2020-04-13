bc303d5bb46d05bfae6ce3d823c4b7b28f62aaee
Biru4015 <rambirendra12@gmail.com>
2020-04-13 16:25:38 +0530

    [Birendra]Adding the first name

---
+++ UserRegistration/UserDeatils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserDetails
+    {
+        public void FirstNameMethod()
+        {
+            Console.Write("Enter your frist name: ");
+            String fname = Console.ReadLine();
+            Regex firstname = new Regex(@"^[A-Z][a-z]{2,}$");
+            if (firstname.IsMatch(fname))
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+        }
+    }
+}
