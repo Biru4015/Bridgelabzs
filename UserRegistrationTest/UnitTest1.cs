c06ee6f6e19ffbcf6fcb5f37b2742317ffa63dd8
Biru4015 <rambirendra12@gmail.com>
2020-04-13 16:56:54 +0530

    [Birendra]Adding the last name for testing

--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace UserRegistrationTest
     /// </summary>
     public class FirstNameTest
     {
+        //--------------------------Test for first name------------------------------
         /// <summary>
         /// This mthod is for wrong input testing
         /// </summary>
@@ -29,5 +30,27 @@ namespace UserRegistrationTest
             UserDetailsTest first = new UserDetailsTest();
             Assert.IsTrue(first.FirstNameMethod("Biru"));
         }
+
+        //--------------------------Test for last name------------------------------
+        /// <summary>
+        /// This mthod is for wrong input testing
+        /// </summary>
+        [Test]
+        public void LastNmaeTest_WorngInput_ReturnsFalse()
+        {
+            UserDetailsTest first = new UserDetailsTest();
+            Assert.IsFalse(first.FirstNameMethod("singh"));
+        }
+
+        /// <summary>
+        /// This method is checking for right input checking
+        /// </summary>
+        [Test]
+        public void LastNameTest_RightInput_ReturnsTrue()
+        {
+            UserDetailsTest first = new UserDetailsTest();
+            Assert.IsTrue(first.FirstNameMethod("Singh"));
+        }
+
     }
 }
--- UserRegistrationTest/UserDetailsTest.cs
+++ UserRegistrationTest/UserDetailsTest.cs
@@ -20,5 +20,19 @@ namespace UserRegistrationTest
             else
                 return false;
         }
+
+        /// <summary>
+        /// This method cantains the code of validating user first name
+        /// </summary>
+        /// <param name="fname"></param>
+        /// <returns>true|false</returns>
+        public bool LastNameMethod(String fname)
+        {
+            Regex firstname = new Regex(@"^[A-Z][a-z]{2,}$");
+            if (firstname.IsMatch(fname))
+                return true;
+            else
+                return false;
+        }
     }
 }
